75e7080b20bccaece4828e902bcdd54efd68adac
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-03-22 00:17:07 +0600

    Add m.equal

--- src/assertions.js
+++ src/assertions.js
@@ -1,10 +1,12 @@
 'use strict'
 
+
 exports.true = bool => {
   if (!bool) {
     throw new Error('not true')
   }
 }
+
 
 exports.throws = fn => {
   let throws = false
@@ -15,5 +17,17 @@ exports.throws = fn => {
   }
   if (!throws) {
     throw new Error('Function does not throw')
+  }
+}
+
+
+const allTrue = (result, n) => result && n
+
+exports.equal = function() {
+  const args = [].slice.call(arguments)
+  const argsEqual = args.map(x => x === args[0]).reduce(allTrue)
+  const isNaNs = args.filter(x => x !== x).length === args.length
+  if (!(argsEqual || isNaNs)) {
+    throw new Error(`${args.join(', ')} are not equal`)
   }
 }
